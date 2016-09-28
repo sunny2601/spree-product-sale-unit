c41889594e9907d2414873daa6afdfbd853e92b4
Edwin Horneij <edwin@astekwallcovering.com>
2016-09-28 16:48:52 -0700

    Declare originals in overrides

--- app/overrides/add_sale_unit_to_product_cart_form.rb
+++ app/overrides/add_sale_unit_to_product_cart_form.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(
+    :original => '72630eb474f87b8212f7faf8901c7b95fcff45e2',
     :virtual_path => 'spree/products/_cart_form',
     :name => 'add_sale_unit_to_product_cart_form',
     :replace => 'span.quantity-info',
