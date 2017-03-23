c0805480cbb3e917efff253f744abd3633085b73
Edwin Horneij <edwin@astekwallcovering.com>
2017-03-23 15:02:21 -0700

    Add set_sale_unit method to Product model

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,11 @@
 module Spree
   Product.class_eval do
     belongs_to :sale_unit
+
+    def set_sale_unit name
+      sale_unit = Spree::SaleUnit.find_by(name: name)
+      self.sale_unit_id = sale_unit.id unless sale_unit.nil?
+      self.save!
+    end
   end
 end
